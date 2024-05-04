cb18dee8498e0a0e63331203d0aca4484d10c02b
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2024-05-03 17:18:06 -0700

    adding latest stuffs

--- src/pages/User-auth.js
+++ src/pages/User-auth.js
@@ -12,16 +12,6 @@ import Logo from '../assets/images/Motivar.svg';
 import Headphone from '../assets/images/headphone.png';
 import Snapback from '../assets/images/snapback.png';
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Image from "react-bootstrap/Image";
-import Logo from "../assets/images/Motivar.svg";
-import Headphone from "../assets/images/headphone.png";
-import Snapback from "../assets/images/snapback.png";
 
 import axios from "axios";
 import toast from "react-hot-toast";
